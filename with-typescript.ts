a712bce6f16d5b9034ff1173e86bd31e19f473ed
Sharon Yu <yu5286pp@gmail.com>
2023-01-05 14:14:19 +0800

    what is typescript

---
+++ with-typescript.ts
@@ -0,0 +1,9 @@
+// function add(a: number, b: number) {
+//     return a + b;
+//   }
+  
+  // the function here does not expect any particular types.
+  
+//   const result = add('2', '5');
+  
+  console.log(result);
